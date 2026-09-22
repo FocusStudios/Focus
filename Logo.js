0f47fe07389a4b826f40ee85f4321158b9c7ee33
Focus <focusstudios2025@gmail.com>
2026-09-22 17:37:04 +0300

    Update Logo.js

--- Logo.js
+++ Logo.js
@@ -196,7 +196,7 @@ const SideNavbar = [
   { link: "https://focusstudios.github.io/SideNavbar/ShadowEffect.html", demo: "https://focusstudios.github.io/SideNavbar-Demo/ShadowEffect.html", title: "Shadow Effect" },
   { link: "https://focusstudios.github.io/SideNavbar/Glassmorphism.html", demo: "https://focusstudios.github.io/SideNavbar-Demo/Glassmorphism.html", title: "Glassmorphism Effect" },
   { link: "https://focusstudios.github.io/SideNavbar/LightDark.html", demo: "https://focusstudios.github.io/SideNavbar-Demo/LightDark.html", title: "Light & Dark Mode" },
-  { link: "https://focusstudios.github.io/SideNavbar/SmartHome.html", demo: "https://focusstudios.github.io/SideNavbar-Demo/SmartHome.html", title: "Smart Home App" },
+  { link: "https://focusstudios.github.io/SideNavbar/SmartHomeApp.html", demo: "https://focusstudios.github.io/SideNavbar-Demo/SmartHomeApp.html", title: "Smart Home App" },
   { link: "https://focusstudios.github.io/SideNavbar/Neumorphism.html", demo: "https://focusstudios.github.io/SideNavbar-Demo/Neumorphism.html", title: "Neumorphism Effect" }
 
 ]
